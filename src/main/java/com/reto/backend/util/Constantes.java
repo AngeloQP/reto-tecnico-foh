6a66ea52e8864a38b3ba823a03279665c45d4420
Angelo QP <angquerevalu@gmail.com>
2024-01-29 23:43:36 -0500

    AQP

--- src/main/java/com/reto/backend/util/Constantes.java
+++ src/main/java/com/reto/backend/util/Constantes.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class Constantes {
 
     public static final String EMPTY_STRING = "";
-    public static final String PATH_ARCHIVO_EXCEL = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "files" + File.separator;
+    public static final String PATH_ARCHIVO_EXCEL = "src" + File.separator + "main" + File.separator + "resources" + File.separator + "archivos" + File.separator;
 
     public static final String MENSAJE_STATUS_OK = "OK";
 }
